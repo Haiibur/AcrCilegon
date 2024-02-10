5bd9dbd511188b1d6cac000dbe31a19608c08886
Haiibur <burhanti2308@gmail.com>
2024-02-10 23:24:57 +0700

    Merubah Halaman Lokasi Tujuan

--- application/modules/Lokasi_Tujuan/views/lokasi_tujuan.php
+++ application/modules/Lokasi_Tujuan/views/lokasi_tujuan.php
@@ -1,99 +1,100 @@
 <div class="row">
-	<div class="col-12 col-md-12 mt-3">
+    <div class="col-12 col-md-12 mt-3">
         <div class="card">
-            <div class="card-body">	      
+            <div class="card-body">
                 <div id="toolbar">
                     <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
-                    <a href="<?=base_url('buat-agenda');?>" class="btn btn-success" title="Buat Agenda">
-                        <i class="fa fa-plus"></i> Agenda Baru
-                    </a>
-                    <a href="<?=base_url('ubah-agenda/');?>" class="btn btn-warning" id="btnRedir" title="Ubah Agenda">
-                        <i class="fa fa-edit"></i> Edit
-                    </a>
-                    <a href="<?=base_url('home/hapusData'); ?>" class="btn btn-danger" id="btnDestroy" title="Hapus Data">
-                        <i class="far fa-trash-alt"></i> Hapus
-                    </a>
+                        <a href="<?=base_url('buat-agenda');?>" class="btn btn-success" title="Buat Agenda">
+                            <i class="fa fa-plus"></i> Tambah Lokasi Tujuan
+                        </a>
+                        <a href="<?=base_url('ubah-agenda/');?>" class="btn btn-warning" id="btnRedir"
+                            title="Ubah Agenda">
+                            <i class="fa fa-edit"></i> Edit
+                        </a>
+                        <a href="<?=base_url('home/hapusData'); ?>" class="btn btn-danger" id="btnDestroy"
+                            title="Hapus Data">
+                            <i class="far fa-trash-alt"></i> Hapus
+                        </a>
                     </div>
                 </div>
                 <div class="table-responsive">
-                    <table
-                    id="table"
-                    class="table table-striped"
-                    data-toggle="table"             
-                    data-toolbar="#toolbar" 
-                    data-pagination="true"
-                    data-search="true"
-                    data-sort-order="desc"
-                    data-id-field="id"
-                    data-page-list="[10, 25, 50, 100, all]"
-                    data-url="<?=base_url('agenda/load_agenda');?>"
-                    >
-                    <thead>
-                        <tr>
-                        <th data-field="state" data-checkbox="true"></th>                
-                        <th data-field="nama" data-sortable="true">Nama Agenda</th>
-                        <th data-field="tamu">Tamu</th>
-                        <th data-field="detail">Detail</th>
-                        <th data-formatter="operateFormatter">Naskah</th>
-                        </tr>
-                    </thead>
+                    <table id="table" class="table table-striped" data-toggle="table" data-toolbar="#toolbar"
+                        data-pagination="true" data-search="true" data-sort-order="desc" data-id-field="id"
+                        data-page-list="[10, 25, 50, 100, all]" data-url="<?=base_url('agenda/load_agenda');?>">
+                        <thead>
+                            <tr>
+                                <th data-field="state" data-checkbox="true"></th>
+                                <th data-field="nama" data-sortable="true">Nama Lokasi</th>
+                                <th data-field="tamu">Keterangan Lokasi</th>
+                                <th data-field="detail">Gambar Lokasi</th>
+                                <th data-field>Link Vidio</th>
+                                <!-- <th data-formatter="operateFormatter">Link Vidio</th> -->
+                            </tr>
+                        </thead>
                     </table>
                 </div>
             </div>
         </div>
-	</div>
+    </div>
 </div>
 
 <div class="modal fade" id="BSmodal" tabindex="-1" role="dialog" aria-labelledby="Modal FormData">
-  <div class="modal-dialog modal-md" role="document">
-    <div class="modal-content">
-      <div class="modal-header backmodal">
-        <h4 class="modal-title">
-          
-        </h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form id="form" action="" method="POST">
-        <div class="modal-body">
-          <input type="hidden" name="id" id="id">
-          <div class="row mb-3">
-            <label class="col-sm-3 col-form-label">Level Admin</label>
-            <div class="col-sm-9">
-              <select class="select2 form-control" name="level" id="level">
-              </select>
+    <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+            <div class="modal-header backmodal">
+                <h4 class="modal-title">
+                </h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-          </div>
-          <div class="row mb-3">
-            <label class="col-sm-3 col-form-label">Username</label>
-            <div class="col-sm-9">
-              <input type="text" class="form-control" name="username" id="username" placeholder="Digunakan untuk Login">
-            </div>
-          </div>
-          <div class="row mb-3">
-            <label class="col-sm-3 col-form-label">Password</label>
-            <div class="col-sm-9">
-              <input type="password" class="form-control" name="password" id="password">
-            </div>
-          </div>
+            <form id="form" action="" method="POST">
+                <div class="modal-body">
+                    <input type="hidden" name="id" id="id">
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">Nama Lokasi</label>
+                        <div class="col-sm-9">
+                            <select class="select2 form-control" name="nama_lokasi" id="nama_lokasi">
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">Keterangan Lokasi</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="keterangan_lokasi" id="keterangan_lokasi"
+                                placeholder="Digunakan untuk Login">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">Gambar Lokasi</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control" name="gambar_lokasi" id="gambar_lokasi">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-3 col-form-label">Link Vidio</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control" name="link_vidio" id="link_vidio">
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Simpan <span id="loading"></span></button>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
+                </div>
+            </form>
         </div>
-        <div class="modal-footer">
-          <button type="submit" class="btn btn-primary">Simpan <span id="loading"></span></button>
-          <button type="button" class="btn btn-danger" data-dismiss="modal">Batal</button>
-        </div>
-      </form>
     </div>
-  </div>
 </div>
 
 <script type="text/javascript">
-	var $table = $('#table')
-	function operateFormatter(value, row, index) {
-    return [
-      '<a href="'+row.naskah+'" class="btn btn-primary">',
-        'Lihat',
-      '</a'
-    ].join('')
-  }  
+var $table = $('#table')
+
+// function operateFormatter(value, row, index) {
+//     return [
+//         '<a href="' + row.naskah + '" class="btn btn-primary">',
+//         'Lihat',
+//         '</a'
+//     ].join('')
+// }
 </script>
