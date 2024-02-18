1136959c18b165aafad9d216e81dd681d26d1242
Haiibur <burhanti2308@gmail.com>
2024-02-18 21:59:32 +0700

    merapihkan sidebar

--- application/views/partials/sidebar.php
+++ application/views/partials/sidebar.php
@@ -3,37 +3,132 @@
         class="sidebarCollapse float-right h6 dropdown-menu-right mr-2 mt-2 position-absolute d-block d-lg-none">
         <i class="icon-close"></i>
     </a>
-
-    <!-- START: Logo-->
     <a href="<?= base_url('home'); ?>" class="sidebar-logo d-flex">
         <img src="<?= base_url('assets/img/logoasistenku.png'); ?>" alt="Asistenku" width="80" class="img-fluid mr-2" />
     </a>
-    <!-- END: Logo-->
-
-    <!-- START: Menu-->
     <ul id="side-menu" class="sidebar-menu">
-        <li><a href="<?= base_url('home'); ?>"><i class="icon-home"></i>Beranda</a>
-        <li><a href="<?= base_url('agenda'); ?>"><i class="fas fa-window-restore"></i>Agenda</a>
-        <li><a href="<?= base_url('Agenda_Acara'); ?>"><i class="fas fa-grip-horizontal"></i>Agenda Acara</a>
-        <li><a href="<?= base_url('Lokasi_venue'); ?>"><i class="icon-location"></i>Lokasi Venue</a>
-        <li><a href="<?= base_url('Hotel'); ?>"><i class="fas fa-synagogue"></i>Hotel</a>
-        <li><a href="<?= base_url('Galleri'); ?>"><i class="fas fa-photo-video"></i>Galleri</a>
-        <li><a href="<?= base_url('Wisata'); ?>"><i class="icon-tree"></i>Wisata</a>
-        <li><a href="<?= base_url('Materi'); ?>"><i class="fas fa-grip-horizontal"></i>Materi</a>
-        <li><a href="<?= base_url('Umdangan'); ?>"><i class="fas fa-grip-horizontal"></i>Undangan</a>
-        <li><a href="<?= base_url('Pendaftaran'); ?>"><i class="fas fa-grip-horizontal"></i>Pendaftaran</a>
-        <li><a href="<?= base_url('Kehadiran'); ?>"><i class="fas fa-grip-horizontal"></i>Kehadiran</a>
-
+        <li>
+            <a href="<?= base_url('home'); ?>">
+                <i class="icon-home"></i>
+                Beranda
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('level_admin'); ?>">
+                <i class="fas fa-grip-horizontal"></i>
+                Level Admin
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Admin'); ?>">
+                <i class="fas fa-grip-horizontal"></i>
+                Admin
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Profil'); ?>">
+                <i class="fas fa-grip-horizontal"></i>
+                Profil
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('agenda'); ?>">
+                <i class="fas fa-grip-horizontal"></i>
+                Agenda Acara
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Provinsi'); ?>">
+                <i class="fas fa-grip-horizontal"></i>
+                Provinsi
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Kabupaten'); ?>">
+                <i class="fas fa-grip-horizontal"></i>
+                Kabupaten
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Materi'); ?>">
+                <i class="fas fa-grip-horizontal"></i>
+                Materi
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Galleri'); ?>">
+                <i class="fas fa-photo-video"></i>
+                Galleri
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Hotel'); ?>">
+                <i class="fas fa-synagogue"></i>
+                Hotel
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('lokasi_tujuan'); ?>">
+                <i class="fas fa-photo-video"></i>
+                Lokasi Tujuan
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('lokasi_vanue'); ?>">
+                <i class="fas fa-photo-video"></i>
+                Lokasi Vanue
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Wisata'); ?>">
+                <i class="icon-tree"></i>
+                Wisata
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('wisata'); ?>">
+                <i class="fas fa-photo-video"></i>
+                Level Peserta
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Umdangan'); ?>">
+                <i class="fas fa-grip-horizontal"></i>
+                Undangan
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Pendaftaran'); ?>">
+                <i class="fas fa-grip-horizontal"></i>
+                Pendaftaran
+            </a>
+        </li>
+        <li>
+            <a href="<?= base_url('Kehadiran'); ?>">
+                <i class="fas fa-grip-horizontal"></i>
+                Kehadiran
+            </a>
+        </li>
         <li class="dropdown">
-            <a href="#"><i class="icon-settings"></i>Pengaturan</a>
+            <a href="#"><i class="icon-settings"></i>
+                Pengaturan
+            </a>
             <div>
                 <ul>
-                    <li><a href="<?= base_url('admin'); ?>"><i class="icon-user"></i> Administrator</a></li>
-                    <li><a href="<?= base_url('profil'); ?>"><i class="fas fa-grip-horizontal"></i> Profil Sistem</a>
+                    <li>
+                        <a href="<?= base_url('admin'); ?>">
+                            <i class="icon-user"></i>
+                            Administrator
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?= base_url('profil'); ?>">
+                            <i class="fas fa-grip-horizontal"></i>
+                            Profil Sistem
+                        </a>
                     </li>
                 </ul>
             </div>
         </li>
     </ul>
-    <!-- END: Menu-->
 </div>
